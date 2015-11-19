afd22a967aa806961b6d943ff3101a245a4e9186
Jordan Hudgens <jordan@wow.com>
2015-11-19 17:13:45 -0600

    Integrated the solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: :index
+
+  # This show route can be refactored into the above resouces' method call above, like so:
+  # resources :students, only: [:index, :show]
+  # However for the sake of this lab and seeing
+  # how you can pass params through the
+  # route, we'll keep it explicit and refactor it later
+  get "students/:id", to: "students#show"
 end
