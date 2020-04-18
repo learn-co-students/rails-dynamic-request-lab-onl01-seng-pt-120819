5671fb0c2940c7cc0bd1b54e665cd4e31944ea0e
Swishyfishie2 <pclandreiuk@gmail.com>
2020-04-19 00:15:56 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show  
+    # binding.pry
+    # render :plain => "Hello World!"
+    # render :plain => params[:id]
+    @student = Student.find_by_id(params[:id])
+  end
 end
