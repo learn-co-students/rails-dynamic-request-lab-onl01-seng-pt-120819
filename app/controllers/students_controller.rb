b830dca11a70a9cb5a2d75d046a22b2c2890450a
Amanda <main@Amandas-MacBook-Air.local>
2020-04-17 21:30:59 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   def index
     @students = Student.all
+  end
+
+  def show 
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
